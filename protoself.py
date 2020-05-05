315c9aba76a28331125427efd747cbff97c43323
Armando Herrera <mail@jellowfish.com>
2020-05-04 20:50:42 -0400

    wip

--- protoself.py
+++ protoself.py
@@ -1,8 +1,28 @@
 def make(**kwargs):
     "..."
     obj = type('',(object,),{})()
-    obj.__dict__ = kwargs
+    obj.__dict__.update(kwargs)
     return obj
 
-orenji = make(name='orenji', color='#FFA500')
-print(orenji.__dict__)
+class Pt:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def __str__(self):
+        try:
+            return str(self)
+        except:
+            print("OHNO")
+        return str(self.__dict__)
+
+    def __setattr__(self, name, data):
+        self.__dict__[name] = data
+
+mikan = Pt(name='mikan', color='214')
+mikan.str = lambda : f"\u001b[38;5;214m{self.name}\u001b[0m"
+
+print(mikan.str())
+
+#mikan = make(name='mikan', color='#ffaf00')
+#print(mikan.__dict__)
+#print(str(mikan))
