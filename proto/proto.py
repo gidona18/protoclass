54db48c675bf5f16c8738404af0fe8f16c291540
Armando Herrera <mail@jellowfish.com>
2020-05-05 18:44:01 -0400

    wip

--- proto/proto.py
+++ proto/proto.py
@@ -1,6 +1,15 @@
 # ---------------------------------------------------------------------
 
+
+from functools import partial
+
+
+# ---------------------------------------------------------------------
+
+
 class Proto:
+    __functype = type(lambda: None)
+
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
         self.__link = None
@@ -18,7 +27,13 @@ class Proto:
             attr = attr.__link
         return self
 
-# ---------------------------------------------------------------------
+    # ---------------------------------------------------------------------
+
+    def __setattr__(self, name, data):
+        if self.__functype == type(data):
+            data = partial(data, self)
+
+        self.__dict__[name] = data
 
     def __getattr__(self, name):
         attr = self
@@ -29,13 +44,28 @@ class Proto:
         exn = "<proto object at {}> has no attribute '{}'>"
         raise AttributeError(exn.format(hex(id(self)), name))
 
-# ---------------------------------------------------------------------
+    # ---------------------------------------------------------------------
 
-    def __str__(self, name):
-        try: return self.__str__()
-        except AttributeError: return self.__dict__.str()
+    def __str__(self):
+        try:
+            return self.str()
+        except AttributeError:
+            return str(self.__dict__)
+
+    def __repr__(self):
+        try:
+            return self.repr()
+        except AttributeError:
+            return repr(self.__dict__)
+
+    def __bytes__(self):
+        try:
+            return self.bytes()
+        except AttributeError:
+            return bytes(self.__dict__)
 
 
+"""
 mikan = Proto(name='mikan', color='11')
 print(mikan.name, mikan.color)
 print()
@@ -51,5 +81,10 @@ print(mikan.name, mikan.color)
 print(orenji.name, orenji.color)
 print(tomato.name, tomato.color)
 print()
+"""
 
+mikan = Proto(name="mikan", color="11")
+print(mikan)
+
+mikan.str = lambda self: "MIKAN!"
 print(mikan)
