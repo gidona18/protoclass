48be25dd789218d9e33cf6ae6836ae80b1d04c2c
Armando Herrera <mail@jellowfish.com>
2020-05-04 20:58:11 -0400

    wip

--- protoself.py
+++ protoself.py
@@ -8,20 +8,23 @@ class Pt:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
-    #def __str__(self):
-    #    try:
-    #        return str(self)
-    #    except:
-    #        print("OHNO")
-    #    return str(self.__dict__)
+    def __str__(self):
+        try:
+            return self.str(self)
+        except AttributeError:
+            return str(self.__dict__)
 
-    def __setattr__(self, name, data):
-        print(repr(data))
+"""
+
+@attr(mikan, __str__)
+def mikan
+
+"""
 
 mikan = Pt(name='mikan', color='214')
-mikan.str = lambda : f"\u001b[38;5;214m{self.name}\u001b[0m"
+mikan.str = lambda self : f"\u001b[38;5;214m{self.name}\u001b[0m"
 
-print(mikan.str())
+print(mikan)
 
 #mikan = make(name='mikan', color='#ffaf00')
 #print(mikan.__dict__)
