89c0001518fd865623982bc4f00896c728b535ad
Armando Herrera <mail@jellowfish.com>
2020-05-05 23:05:58 -0400

    everything is broken

--- proto/proto2.py
+++ proto/proto2.py
@@ -8,52 +8,61 @@ class __prot:
     def __getattr__(self, name):
         return getattr(type(self), name)
 
+# ---------------------------------------------------------------------
 
-def __link(self, other):
-    class prot(__prot):
-        def __init__(self):
-            pass
-
-    prot = type('prot', (type(other),), prot.__dict__.copy())
-    return prot()
-
-
-def __copy(self, other):
+def link(self, other):
     class prot:
-        def __init__(self):
-            pass
-        
+        def __init__(self, __dict):
+            for name in __dict:
+                setattr(type(self), name, __dict[name])
         def __setattr__(self, name, data):
             return setattr(type(self), name, data)
         
         def __getattr__(self, name):
             return getattr(type(self), name)
 
+    prot(type(self).__dict__)
     prot = type('prot', (type(other),), prot.__dict__.copy())
     return prot()
 
 
-def prot(**kwargs):
+def dupe(self, other):
     class prot(__prot):
+        def __init__(self):
+            pass
+
+    prot_dict = type(other).__dict__.copy()
+    #prot = type('prot', (type(other),), prot.__dict__.copy())
+    #prot_link = getattr(type(other), '__prot_link')
+    #while prot_link:
+
+    prot_dict.update(type(self))
+    setattr(prot, '__prot_link', None)
+    return prot()
+
+
+def prot(**kwargs):
+    class prot:
         def __init__(self, __dict):
             for name in __dict:
                 setattr(type(self), name, __dict[name])
+        def __setattr__(self, name, data):
+            return setattr(type(self), name, data)
+        
+        def __getattr__(self, name):
+            return getattr(type(self), name)
             
-    setattr(prot, 'link', MethodType(__link, prot))
-    setattr(prot, 'copy', MethodType(__copy, prot))
+    setattr(prot, 'link', MethodType(link, prot))
+    setattr(prot, 'dupe', MethodType(dupe, prot))
+    setattr(prot, '__prot_link', None)
     return prot(kwargs)
 
 
+arm = prot(name='arm')
+print(arm.name)
+print()
 
+cla = prot(age='22').link(arm)
+print(cla.age)
+print()
 
-jude = prot(name='jude')
-jay = prot().link(jude)
-print(jay.name)
-
-jude.name = 'jude2'
-print(jay.name)
-
-jay.name = 'jay'
-jude.name = 'jude'
-print(jude.name)
-print(jay.name)
