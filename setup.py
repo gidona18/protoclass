8470551ae48b9d52d44cc446ddc6d2fffc3b0f55
Armando Herrera <mail@jellowfish.com>
2020-05-06 15:04:59 -0400

    setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='proto',
-    version='0.0.1',
+    name="pyproto",
+    version="0.0.1",
     packages=find_packages(),
+    author="Armando Herrera",
+    author_email="[email protected]",
+    description="TODO",
+    #long_description="TODO",
+    keywords="TODO",
+    url="https://github.com/jellowfish/pyproto",
 )
