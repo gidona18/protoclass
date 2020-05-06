aabe1007f7a214f0c31295fd5f138726943c8bbd
Armando Herrera <mail@jellowfish.com>
2020-05-06 00:42:01 -0400

    now everything works

--- proto/proto3.py
+++ proto/proto3.py
@@ -1,12 +1,3 @@
-class X:
-    def __init__(self, x):
-        self.x = x
-
-x = X(10)
-x.put = (lambda self: print(self.x)).__get__(x)
-
-#x.put()
-
 
 class __prot:
     def link(self, other):
@@ -14,8 +5,6 @@ class __prot:
         return prot()
 
     def __setattr__(self, name, data):
-        if type(data) == type(lambda:None):
-            data = data.__get__(self)#XXX:DOES NOT WORK
         setattr(type(self), name, data)
 
     def __getattr__(self, name):
@@ -25,14 +14,13 @@ class __prot:
 def prot(**kwargs):
     return type('prot', (__prot,), kwargs.copy())()
 
+
 mikan = prot(name='mikan')
-#print(mikan.name)
-mikan.__str__ = lambda self: mikan.name
+mikan.__str__ = lambda self: self.name
 print(mikan)
 
 ajo = prot(name='ajo').link(mikan)
 print(ajo)
-#mikan.__str__.__set__(None)
-#ajo.__str__ = mikan.__str__.unbind()
-#print(ajo)
-#print(dir(ajo.__str__))
+
+mikan.put = lambda self: print(self)
+ajo.put()
