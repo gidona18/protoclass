17917115fccf5681f3a1e068d25318a5422e92a0
Armando Herrera <mail@jellowfish.com>
2020-05-05 21:45:39 -0400

    wip

--- proto/proto2.py
+++ proto/proto2.py
@@ -1,14 +1,33 @@
-class Prot:
-    def __init__(self):
-        pass
-    def __call__(self, **kwargs):
-        class prot(Prot):
-            def __init__(self, __dict):
-                self.__dict__.update(__dict)
-        return prot(kwargs)
+from types import MethodType
 
-prot = Prot()
+def __soft(self, other):
+    pass
+
+def __hard(self, other):
+    class prot(other):
+        def __getattr__(self, name):
+            return "OHNO"
+    return prot
+
+def prot(**kwargs):
+    class prot:
+        def __init__(self, __dict):
+            for name in __dict:
+                setattr(type(self), name, __dict[name])
+        def __setattr__(self, name, data):
+            return setattr(type(self), name, data)
+        def __getattr__(self, name):
+            return getattr(type(self), name)
+    return prot(kwargs)
+
+
+
+
 
 jude = prot(name='jude')
 john = prot(name='john')
-print(type(jude) == type(john))
+#jude.age = 21
+print(jude.name)
+
+#j = prot().hard(jude)
+#print(j.name)
