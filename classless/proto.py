551b2d03d956caf3055891b9be9a84b83c18dc7e
Armando Herrera <mail@jellowfish.com>
2020-05-06 19:23:35 -0400

    docs

--- classless/proto.py
+++ classless/proto.py
@@ -2,8 +2,16 @@
 
 
 class __proto:
+    "Base prototype"
     def link(self, other):
-        type(self).__bases__ = (type(other),)
+        """Make current object a prototype of another object.
+        This is similar to single inheritance. Each object
+        can have multiple child links, but only one parent link.
+        Parent links can be changed at any time, but objects
+        that are children of this one will be affected by this.
+        This enables change propagation.
+        """
+        type(self).__bases__ = (type(other),)  # change parent link
         return self
 
     def __setattr__(self, name, value):
@@ -14,6 +22,10 @@ class __proto:
 
 
 def proto(**kwargs):
+    """Make a new prototype object with the given attributes.
+    If no properties are given, and empty prototype will be created.
+    The latter is useful when only a clone of another object is needed.
+    """
     return type('proto', (__proto,), kwargs.copy())()
 
 
