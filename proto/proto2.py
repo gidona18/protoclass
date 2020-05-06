1e5377f01591daf789a33bff49fdf439b34cb020
Armando Herrera <mail@jellowfish.com>
2020-05-05 21:12:54 -0400

    another approach

---
+++ proto/proto2.py
@@ -0,0 +1,13 @@
+class Prot:
+    def __init__(self):
+        pass
+    def __call__(self, **kwargs):
+        class prot(Prot):
+            def __init__(self, __dict):
+                self.__dict__.update(__dict)
+        return prot(kwargs)
+
+prot = Prot()
+
+jude = prot(name='jude')
+print(jude.name)
