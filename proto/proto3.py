7528f43c13c5bea4eb43b253f1dc8ddf03a89626
Armando Herrera <mail@jellowfish.com>
2020-05-06 00:34:43 -0400

    gb

--- proto/proto3.py
+++ proto/proto3.py
@@ -5,5 +5,34 @@ class X:
 x = X(10)
 x.put = (lambda self: print(self.x)).__get__(x)
 
-x.put()
+#x.put()
 
+
+class __prot:
+    def link(self, other):
+        prot = type('prot', (type(other),), type(self).__dict__.copy())
+        return prot()
+
+    def __setattr__(self, name, data):
+        if type(data) == type(lambda:None):
+            data = data.__get__(self)#XXX:DOES NOT WORK
+        setattr(type(self), name, data)
+
+    def __getattr__(self, name):
+        return getattr(type(self), name)
+
+
+def prot(**kwargs):
+    return type('prot', (__prot,), kwargs.copy())()
+
+mikan = prot(name='mikan')
+#print(mikan.name)
+mikan.__str__ = lambda self: mikan.name
+print(mikan)
+
+ajo = prot(name='ajo').link(mikan)
+print(ajo)
+#mikan.__str__.__set__(None)
+#ajo.__str__ = mikan.__str__.unbind()
+#print(ajo)
+#print(dir(ajo.__str__))
