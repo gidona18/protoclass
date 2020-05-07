8b5977d60aec9ef716e15f5a795d49cc184cd974
Armando Herrera <mail@jellowfish.com>
2020-05-06 20:47:15 -0400

    fixed repr issue

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="protoclass",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(exclude=["tests"]),
     description="Prototype-Oriented Programming in Python",
     long_description=README,
