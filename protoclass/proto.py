c8897152091642b688063b44a9c08130c43aee5d
Armando Herrera <mail@jellowfish.com>
2020-05-08 01:05:58 -0400

    docstring

--- protoclass/proto.py
+++ protoclass/proto.py
@@ -32,13 +32,46 @@ def proto(**attrs):
     return ty()
 
 
-def clone(other):
-    """..."""
+def clone(prototype):
+    """
+    """
+
     return proto().chain(other)
 
 
-def multiclone(*others):
-    return proto().multichain(*others)
+def multiclone(*proto_objects):
+    """Make a new object that inherits from multiple prototypes.
+
+    Equivalent to proto().multichain(*`proto_objects`).
+    When the parent prototypes share attributes with the same name, the
+    parent prototype that is first in the list of prototypes will
+    provide it.
+
+    Parameters
+    ----------
+    *proto_objects : iterable
+        Prototypes to inherit attributes from.
+
+    Returns
+    -------
+    proto : proto
+        A new object that inherits from multiple prototypes.
+
+    Examples
+    --------
+    >>> cat = proto(name="cat", meow="meow")
+    >>> dog = proto(name="dog", bark="woof")
+    >>> catdog = multiclone(cat, dog)
+    >>> catdog.meow
+    "meow"
+    >>> catdog.bark
+    "woof"
+    >>> catdog.name  # first attribute found will be used upon conflict
+    "cat"
+
+    """
+
+    return proto().multichain(*proto_objects)
 
 
 # what if some objects should not be cloned?
