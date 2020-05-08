cef066ad6b7591487830427401c031083200640b
Armando Herrera <mail@jellowfish.com>
2020-05-07 23:18:37 -0400

    wip

--- protoclass/proto.py
+++ protoclass/proto.py
@@ -1,19 +1,15 @@
 # ---------------------------------------------------------------------
 
 
-class __proto:
+class __Proto:
     "Base prototype"
 
-    def link(self, other):
-        """Make current object a prototype of another object.
-        This is similar to single inheritance. Each object
-        can have multiple child links, but only one parent link.
-        Parent links can be changed at any time, but objects
-        that are children of this one will be affected by this.
-        This enables change propagation.
-        """
-        type(self).__bases__ = (type(other),)  # change parent link
+    def chain(self, other):
+        """..."""
+        type(self).__bases__ = (type(other),)
         return self
+
+    #def multichain(self, other):
 
     def __setattr__(self, name, value):
         return setattr(type(self), name, value)
@@ -23,11 +19,14 @@ class __proto:
 
 
 def proto(**kwargs):
-    """Make a new prototype object with the given attributes.
-    If no properties are given, and empty prototype will be created.
-    The latter is useful when only a clone of another object is needed.
-    """
-    return type("proto", (__proto,), kwargs.copy())()
+    """..."""
+    return type("proto", (__Proto,), kwargs.copy())()
+
+#def clone(proto_object):
+#    """..."""
+#    return proto().chain(proto_object)
+
+# what if some objects should not be cloned?
 
 
 # ---------------------------------------------------------------------
