fa6fbe78f0490ec8cb2389e71938e36344634037
Armando Herrera <mail@jellowfish.com>
2020-05-11 23:33:24 -0400

    bumpversion

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,21 +12,21 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
+
+sys.path.insert(0, os.path.abspath("../../"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'protoclass'
-copyright = '2020, Armando Herrera'
-author = 'Armando Herrera'
+project = "protoclass"
+copyright = "2020, Armando Herrera"
+author = "Armando Herrera"
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = "0.5.3"
 
-# Fix problem with rtd
-master_doc = 'index'
-
+# readthedocs
+master_doct = "index"
 
 # -- General configuration ---------------------------------------------------
 
@@ -34,11 +34,11 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.napoleon',
+    "sphinx.ext.napoleon",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -51,9 +51,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
