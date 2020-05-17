ed5b3cba95cea8044b66d74bea11d49953e0a7c9
Armando Herrera <mail@jellowfish.com>
2020-05-16 20:56:51 -0400

    style init

--- protoclass/__init__.py
+++ protoclass/__init__.py
@@ -1,22 +1,21 @@
 # ---------------------------------------------------------------------
-# Current
-# ---------------------------------------------------------------------
-
-
-from .proto import proto, clone, chain
-
-
-# ---------------------------------------------------------------------
-# Deprecated
-# ---------------------------------------------------------------------
-
-
-from .proto import multiclone, multichain
-
-
-# ---------------------------------------------------------------------
-# Metadata
+# protoclass
 # ---------------------------------------------------------------------
 
 
 __version__ = "0.5.4"
+
+
+from .proto import proto, clone, chain
+
+proto = proto
+clone = clone
+chain = chain
+
+
+# XXX: deprecated in 0.5.3, removed in 1.0.0
+
+from .proto import multiclone, multichain
+
+multiclone = multiclone
+multichain = multichain
